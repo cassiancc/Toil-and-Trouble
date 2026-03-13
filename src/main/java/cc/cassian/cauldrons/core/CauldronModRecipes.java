003e9d7f008415d0683ece258f719504885e084f
Cassian <cassian@godsted.com>
2026-03-13 16:40:49 -0400

    rename alchemy recipe type

--- src/main/java/cc/cassian/cauldrons/core/CauldronModRecipes.java
+++ src/main/java/cc/cassian/cauldrons/core/CauldronModRecipes.java
@@ -5,17 +5,18 @@ import cc.cassian.cauldrons.recipe.AlchemyRecipe;
 import cc.cassian.cauldrons.recipe.BrewingRecipe;
 import cc.cassian.cauldrons.recipe.InsertingRecipe;
 import cc.cassian.cauldrons.registry.CommonRegistry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
 
 public class CauldronModRecipes {
     public static final RecipeType<BrewingRecipe> BREWING = CommonRegistry.registerRecipe("brewing", () -> registerRecipeType("brewing"));
-    public static final RecipeType<AlchemyRecipe> ALCHEMY = CommonRegistry.registerRecipe("dipping", () -> registerRecipeType("dipping"));
+    public static final RecipeType<AlchemyRecipe> ALCHEMY = CommonRegistry.registerRecipe("alchemy", () -> registerRecipeType("alchemy"));
     public static final RecipeType<InsertingRecipe> INSERTING = CommonRegistry.registerRecipe("inserting", () -> registerRecipeType("inserting"));
 
     public static final RecipeSerializer<BrewingRecipe> BREWING_SERIALIZER = CommonRegistry.registerRecipeSerializer("brewing", ()-> new RecipeSerializer<>(BrewingRecipe.CODEC, BrewingRecipe.STREAM_CODEC));
-    public static final RecipeSerializer<AlchemyRecipe> ALCHEMY_SERIALIZER = CommonRegistry.registerRecipeSerializer("dipping", ()-> new RecipeSerializer<>(AlchemyRecipe.CODEC, AlchemyRecipe.STREAM_CODEC));
+    public static final RecipeSerializer<AlchemyRecipe> ALCHEMY_SERIALIZER = CommonRegistry.registerRecipeSerializer("alchemy", ()-> new RecipeSerializer<>(AlchemyRecipe.CODEC, AlchemyRecipe.STREAM_CODEC));
     public static final RecipeSerializer<InsertingRecipe> INSERTION_SERIALIZER = CommonRegistry.registerRecipeSerializer("inserting", ()-> new RecipeSerializer<>(InsertingRecipe.CODEC, InsertingRecipe.STREAM_CODEC));
 
 
@@ -28,7 +29,8 @@ public class CauldronModRecipes {
     }
 
     public static void touch() {
-
+        BuiltInRegistries.RECIPE_TYPE.addAlias(CauldronMod.of("dipping"), CauldronMod.of("alchemy"));
+        BuiltInRegistries.RECIPE_SERIALIZER.addAlias(CauldronMod.of("dipping"), CauldronMod.of("alchemy"));
     }
 
 }
