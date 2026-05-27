6211ecfdfd26fe54111fb3fa6d9de11cdf14ebca
Cassian <cassian@godsted.com>
2026-05-27 17:05:20 -0400

    Port to 26.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,6 +23,7 @@ stonecutter {
                version("$version-$it", version).buildscript = "build.$it.gradle.kts"
             }
         match("26.1", "fabric", "neoforge")
+        match("26.2", "fabric")
 
         vcsVersion = "26.1-fabric"
     }
